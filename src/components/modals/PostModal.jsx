038748d19f3f8273e222c475b28f66287f7408db
iamsamirtagiyev <stagiyev579@gmail.com>
2024-04-12 20:54:27 +0400

    post

--- src/components/modals/PostModal.jsx
+++ src/components/modals/PostModal.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoClose } from "react-icons/io5";
+import { FaImage } from "react-icons/fa6";
 
 const PostModal = () => {
+  const [post, setPost] = useState()
   return (
     <div className='flex flex-col gap-3'>
-        <label className='relative'>
-            <div className='absolute top-1 right-1 text-2xl cursor-pointer hover:text-red-500 transition-all duration-500'>
-                <IoClose/>
-            </div>
-            <input type="file" accept='images/*, video/*' className='hidden' />
-            <div className="w-full">
-                <img className='w-full h-40 object-cover rounded-md' src="https://i.pinimg.com/736x/98/d0/72/98d0722dc446aee1d5818b1fd4de2dc3.jpg" alt="" />
-            </div>
-        </label>
-        <input type="text" className="text-xl font-medium rounded w-full px-3 py-1.5" placeholder='Description' />
-        <button className="w-full bg-indigo-600 transition-all duration-500 hover:bg-indigo-800 rounded px-3 py-1.5 text-xl font-medium">Send Post</button>
+      <label className='w-full h-52 rounded relative'>
+        { post && <button className='absolute top-2 right-2 z-20 bg-red-500 rounded text-2xl' onClick={() => setPost()}>
+          <IoClose/>
+        </button> }
+        <input type="file" accept='image/*, video/*' className='hidden' onChange={e => setPost(URL.createObjectURL(e.target.files[0]))} />
+        <div className="w-full h-full bg-zinc-400 rounded flex items-center justify-center text-zinc-600">
+         { post ? <img src={post} className='w-full h-full object-cover rounded' alt="" /> : <FaImage size={35}/>}
+        </div>
+      </label>
     </div>
   )
 }
