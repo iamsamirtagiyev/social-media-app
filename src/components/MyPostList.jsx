8bf078b91b8db256dcb63570d414724dfd10cdcf
iamsamirtagiyev <stagiyev579@gmail.com>
2024-04-28 06:28:46 +0400

    start profile page

--- src/components/MyPostList.jsx
+++ src/components/MyPostList.jsx
@@ -4,8 +4,20 @@ import MyPost from './MyPost'
 const MyPostList = () => {
   return (
     <div>
-        <h1 className='text-3xl font-bold ml-5'>All Posts</h1>
-        <div className='grid grid-cols-3 gap-1 p-1 mt-8'>
+      <h1 className='text-3xl font-bold ml-5 inline-block my-2'>All Posts</h1>
+      <hr />
+      <div className='grid grid-cols-3 gap-1 p-1 mt-8 h-full grid-rows-3'>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
+        <MyPost/>
         <MyPost/>
         <MyPost/>
         <MyPost/>
--- src/pages/layout/Profile.jsx
+++ src/pages/layout/Profile.jsx
@@ -6,7 +6,7 @@ import MyPostList from '../../components/MyPostList'
 const Profile = () => {
   const { theme } = useSelector(state => state.theme)
   return (
-    <div data-theme={ theme } className='w-full bg-zinc-800 rounded sm:p-2 h-full overflow-auto'>
+    <div data-theme={ theme } className='w-full profile rounded sm:p-2 h-full overflow-auto'>
       <ProfileInfo/> 
       <MyPostList/>
     </div>
